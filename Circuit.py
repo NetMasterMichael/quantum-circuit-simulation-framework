Fix floating point precision errors

Circuit.py
@@ -2,6 +2,8 @@ import numpy as np
 
 class Circuit:
 
+    # Floating point precision for rounding, since this is running on a classical computer after all, which are famously bad at division
+    FP_PRECISION = 15 
     HADAMARD = (1 / np.sqrt(2)) * np.array([[1, 1], [1, -1]])
 
     def __init__(self, qubits):
@@ -14,11 +16,15 @@ class Circuit:
     def set_qubit(self, qubit_index, new_state):
         # Validate that it is a valid quantum state
         # Condition: it's modulus/length must be equal (or close, to account for FP errors) to 1
-        if np.isclose(np.sqrt(np.sum(new_state * new_state)), 1) and new_state.shape == (2,):
+        if round(np.sqrt(np.sum(new_state * new_state))) == 1 and new_state.shape == (2,):
             self._qubits[qubit_index] = new_state
             return True
         else:
             return False
         
+    def round(self, qubit_state):
+        return np.round(qubit_state, decimals=Circuit.FP_PRECISION)
+        
     def hadamard(self, qubit_index):
-        self._qubits[qubit_index] = np.dot(Circuit.HADAMARD, self._qubits[qubit_index])
+        new_state = self.round(np.dot(Circuit.HADAMARD, self._qubits[qubit_index]))
+        self._qubits[qubit_index] = new_state