6c3b020c38024f50f082507da3b5c56e436588f9
Michael Goodwin <michael.cgdwn@gmail.com>
2025-05-23 23:38:07 +0100

    Rename _qubits array to _circuit_state in Circuit.py

--- Circuit.py
+++ Circuit.py
@@ -9,47 +9,47 @@ class Circuit:
 
     def __init__(self, qubits):
         # Setup basis vector of qubit states
-        self._total_qubits = qubits
-        self._qubits = np.zeros(2 ** qubits)
+        self._total_circuit_state = qubits
+        self._circuit_state = np.zeros(2 ** qubits)
         # Set circuit state to |0> (tensored with # of qubits)
-        self._qubits[0] = 1
+        self._circuit_state[0] = 1
 
-    def get_qubit(self, qubit_index):
+    def get_state(self, index):
         # Note: It is currently impossible to observe a state in quantum mechanics without it collapsing! To be used for debugging only
-        return self._qubits[qubit_index]
+        return self._circuit_state[index]
     
-    def get_all_qubits(self):
-        return self._qubits
+    def get_circuit_state(self):
+        return self._circuit_state
 
-    def set_qubit(self, qubit_index, new_state):
+    def set_qubit(self, index, new_state):
         # Validate that it is a valid quantum state
         # Condition: it's modulus/length must be equal (or close, to account for FP errors) to 1
         if round(np.sqrt(np.sum(new_state * new_state))) == 1 and new_state.shape == (2,):
-            self._qubits[qubit_index] = new_state
+            self._circuit_state[qubit_index] = new_state
             return True
         else:
             return False
         
-    def get_total_qubits(self):
-        return self._total_qubits
+    def get_total_circuit_state(self):
+        return self._total_circuit_state
         
     def round(self, qubit_state):
         return np.round(qubit_state, decimals=Circuit.FP_PRECISION)
         
     def hadamard(self, qubit_index):
-        new_state = self.round(np.dot(Circuit.HADAMARD, self._qubits[qubit_index]))
-        self._qubits[qubit_index] = new_state
+        new_state = self.round(np.dot(Circuit.HADAMARD, self._circuit_state[qubit_index]))
+        self._circuit_state[qubit_index] = new_state
 
     def hadamard_range(self, start_qubit_index, end_qubit_index):
         for i in range(start_qubit_index, end_qubit_index + 1):
             self.hadamard(i)
 
     def hadamard_all(self):
-        for i in range(0, self._total_qubits):
+        for i in range(0, self._total_circuit_state):
             self.hadamard(i)
 
     def measure_qubit(self, qubit_index):
-        qubit_state = self._qubits[qubit_index]
+        qubit_state = self._circuit_state[qubit_index]
         # Apply Born's Rule
         qubit_state = [qubit_state[0] ** 2, qubit_state[1] ** 2]
         # Collapse qubit to either |0> or |1>
@@ -57,9 +57,9 @@ class Circuit:
         collapse = random.uniform(0, probabilities_sum)
         if collapse > qubit_state[0]:
             # Collapse to |1>
-            self._qubits[qubit_index] = np.array([0, 1])
+            self._circuit_state[qubit_index] = np.array([0, 1])
             return np.array([0, 1])
         else:
             # Collapse to |0>
-            self._qubits[qubit_index] = np.array([1, 0])
+            self._circuit_state[qubit_index] = np.array([1, 0])
             return np.array([1, 0])
