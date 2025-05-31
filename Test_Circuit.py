6d8f459e88d5d3246a30c02f80326c9afc4828f4
Michael Goodwin <michael.cgdwn@gmail.com>
2025-05-31 23:11:34 +0100

    Update Test_Circuit to repeat tests up to an upper bound of qubits

--- Test_Circuit.py
+++ Test_Circuit.py
@@ -2,10 +2,17 @@ import unittest
 import numpy as np
 import Circuit
 
+# For repeating tests, set the max qubits that the tests will repeat up until
+MAX_QUBITS = 8
+
 class Test_Circuit(unittest.TestCase):
+
     def test_initialise_circuit(self):
-        qubits = 1
-        testCircuit = Circuit.Circuit(qubits)
-        expectedState = np.zeros(2 ** qubits, dtype = complex)
-        expectedState[0] = 1 + 0j
-        self.assertTrue(np.array_equal(testCircuit.get_circuit_state(), expectedState))
+        for i in range(1, MAX_QUBITS + 1):
+            qubits = i
+            testCircuit = Circuit.Circuit(qubits)
+            expectedState = np.zeros(2 ** qubits, dtype = complex)
+            expectedState[0] = 1 + 0j
+            self.assertTrue(np.array_equal(testCircuit.get_circuit_state(), expectedState), 
+                msg = f"test_initialise_circuit: Test failed with {i} qubits;\nExpected: {expectedState}\nActual: {testCircuit.get_circuit_state()}")
+            print(f"test_initialise_circuit: Test passed with {i} qubits")
