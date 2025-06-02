04db69a2aec622ca9790d8d1739f9485c653252b
Michael Goodwin <michael.cgdwn@gmail.com>
2025-06-02 01:59:04 +0100

    Split Pauli X tests into start, end and uniform in Test_Circuit

--- Test_Circuit.py
+++ Test_Circuit.py
@@ -33,7 +33,37 @@ class Test_Circuit(unittest.TestCase):
                 msg = f"test_initialise_circuit: Test failed with {i} qubits;\nExpected: {expectedState}\nActual: {testCircuit.get_circuit_state()}")
             print(f"test_initialise_circuit: Test passed with {i} qubits")
 
-    def test_pauli_X(self):
+    def test_pauli_X_start(self):
+        for i in range(1, MAX_QUBITS + 1):
+            qubits = i
+            testCircuit = Circuit.Circuit(qubits)
+            zero_state = generate_ket_0_state(qubits)
+            expectedState = zero_state
+            U = PAULI_X
+            for j in range(1, i):
+                U = np.kron(U, IDENTITY)
+            expectedState = np.dot(U, expectedState)
+            testCircuit.apply_operator("X0")
+            self.assertTrue(np.array_equal(testCircuit.get_circuit_state(), expectedState), 
+                msg = f"test_pauli_X_start: Test failed with {i} qubits;\nExpected: {expectedState}\nActual: {testCircuit.get_circuit_state()}")
+            print(f"test_pauli_X_start: Test passed with {i} qubits")
+    
+    def test_pauli_X_end(self):
+        for i in range(2, MAX_QUBITS + 1):
+            qubits = i
+            testCircuit = Circuit.Circuit(qubits)
+            expectedState = generate_ket_0_state(qubits)
+            U = np.array([1], dtype = complex)
+            for j in range(0, i - 1):
+                U = np.kron(U, IDENTITY)
+            U = np.kron(U, PAULI_X)
+            expectedState = np.dot(U, expectedState)
+            testCircuit.apply_operator("X" + str(i - 1))
+            self.assertTrue(np.array_equal(testCircuit.get_circuit_state(), expectedState), 
+                msg = f"test_pauli_X_end: Test failed with {i} qubits;\nExpected: {expectedState}\nActual: {testCircuit.get_circuit_state()}")
+            print(f"test_pauli_X_end: Test passed with {i} qubits")
+
+    def test_uniform_pauli_X(self):
         for i in range(1, MAX_QUBITS + 1):
             qubits = i
             testCircuit = Circuit.Circuit(qubits)
@@ -45,5 +75,5 @@ class Test_Circuit(unittest.TestCase):
             U_key = generate_single_gate_circuit_DSL("X", qubits)
             testCircuit.apply_operator(U_key)
             self.assertTrue(np.array_equal(testCircuit.get_circuit_state(), expectedState), 
-                msg = f"test_pauli_X: Test failed with {i} qubits;\nExpected: {expectedState}\nActual: {testCircuit.get_circuit_state()}")
-            print(f"test_pauli_X: Test passed with {i} qubits")
+                msg = f"test_uniform_pauli_X: Test failed with {i} qubits;\nExpected: {expectedState}\nActual: {testCircuit.get_circuit_state()}")
+            print(f"test_uniform_pauli_X: Test passed with {i} qubits")
