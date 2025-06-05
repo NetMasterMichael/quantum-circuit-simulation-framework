705ab8951fccef038723047e5a596dd3c0f06207
Michael Goodwin <michael.cgdwn@gmail.com>
2025-06-05 23:04:45 +0100

    Refactor imports and declarations in Test_Circuit

--- Test_Circuit.py
+++ Test_Circuit.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-import Circuit
+from Circuit import Circuit
 
 # For repeating tests, set the max qubits that the tests will repeat up until
 MAX_QUBITS = 12
@@ -40,7 +40,7 @@ class Test_Circuit(unittest.TestCase):
     def test_initialise_circuit(self):
         for i in range(1, MAX_QUBITS + 1):
             qubits = i
-            testCircuit = Circuit.Circuit(qubits)
+            testCircuit = Circuit(qubits)
             expectedState = generate_ket_0_state(qubits)
             self.assertTrue(np.array_equal(testCircuit.get_circuit_state(), expectedState), 
                 msg = f"test_initialise_circuit: Test failed with {i} qubits;\nExpected: {expectedState}\nActual: {testCircuit.get_circuit_state()}")
@@ -48,13 +48,13 @@ class Test_Circuit(unittest.TestCase):
 
     def test_invalid_qubits(self):
         # Test zero
-        self.assertRaises(ValueError, Circuit.Circuit, 0)
+        self.assertRaises(ValueError, Circuit, 0)
         # Test negative
-        self.assertRaises(ValueError, Circuit.Circuit, -1)
+        self.assertRaises(ValueError, Circuit, -1)
         print(f"test_invalid_qubits: Test passed")
 
     def test_invalid_gates(self):
-        testCircuit = Circuit.Circuit(2)
+        testCircuit = Circuit(2)
         # Test a syntactically incorrect operator
         self.assertRaises(ValueError, testCircuit.apply_operator, "")
         self.assertRaises(ValueError, testCircuit.apply_operator, "H")
@@ -69,7 +69,7 @@ class Test_Circuit(unittest.TestCase):
         for gate in SINGLE_QUBIT_GATES:
             for i in range(1, MAX_QUBITS + 1):
                 qubits = i
-                testCircuit = Circuit.Circuit(qubits)
+                testCircuit = Circuit(qubits)
                 zero_state = generate_ket_0_state(qubits)
                 expectedState = zero_state
                 U = SINGLE_QUBIT_GATES[gate]
@@ -85,7 +85,7 @@ class Test_Circuit(unittest.TestCase):
         for gate in SINGLE_QUBIT_GATES:
             for i in range(1, MAX_QUBITS + 1):
                 qubits = i
-                testCircuit = Circuit.Circuit(qubits)
+                testCircuit = Circuit(qubits)
                 expectedState = generate_ket_0_state(qubits)
                 U = np.array([1], dtype = complex)
                 for j in range(0, i - 1):
@@ -101,7 +101,7 @@ class Test_Circuit(unittest.TestCase):
         for gate in SINGLE_QUBIT_GATES:
             for i in range(1, MAX_QUBITS + 1):
                 qubits = i
-                testCircuit = Circuit.Circuit(qubits)
+                testCircuit = Circuit(qubits)
                 expectedState = generate_ket_0_state(qubits)
                 U = np.array([1], dtype = complex)
                 for j in range(0, i):
@@ -116,7 +116,7 @@ class Test_Circuit(unittest.TestCase):
     def test_measurement(self):
         for i in range(1, MAX_QUBITS + 1):
             qubits = i
-            testCircuit = Circuit.Circuit(qubits)
+            testCircuit = Circuit(qubits)
             # testCircuit is initialised as |0..0>, check that we measure zero
             expectedState = generate_ket_0_state(qubits)
             testCircuit.measure()
@@ -146,7 +146,7 @@ class Test_Circuit(unittest.TestCase):
             test_passed = False
             reruns = 5
             while reruns != 0:
-                testCircuit = Circuit.Circuit(qubits, operator_cache = True)
+                testCircuit = Circuit(qubits, operator_cache = True)
                 seen_states = set()
                 for j in range(0, int(samples)):
                     testCircuit.reset_circuit_state()
@@ -170,7 +170,7 @@ class Test_Circuit(unittest.TestCase):
     def test_partial_superposition_measurement(self):
         for i in range(2, MAX_QUBITS + 1):
             qubits = i
-            testCircuit = Circuit.Circuit(qubits, operator_cache = True)
+            testCircuit = Circuit(qubits, operator_cache = True)
             repeats = 10
             accepted_0_state = np.zeros(2 ** qubits, dtype = complex)
             accepted_0_state[0] = 1 + 0j
