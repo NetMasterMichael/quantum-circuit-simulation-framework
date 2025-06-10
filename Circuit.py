7f68490381bebc0816628886882a0a932854cad1
Michael Goodwin <michael.cgdwn@gmail.com>
2025-06-10 18:53:49 +0100

    Add logic for tokenization, validation, ordering, padding and parsing to apply_key_preprocessing in Circuit

--- Circuit.py
+++ Circuit.py
@@ -3,6 +3,7 @@ import re
 
 class Circuit:
 
+
     def __init__(self, qubits: int, operator_cache: bool = False, hardware_mode: str = 'CPU'):
         if qubits < 1:
             raise ValueError("Qubits parameter must be at least 1, got " + str(qubits))
@@ -36,9 +37,11 @@ class Circuit:
             'Z': self.PAULI_Z,
             'H': self.HADAMARD
         }
+
     
     def get_circuit_state(self):
         return self._circuit_state
+
 
     def reset_circuit_state(self):
         np = self.np
@@ -46,10 +49,12 @@ class Circuit:
         self._circuit_state = np.zeros(2 ** self._qubits, dtype = complex)
         self._circuit_state[0] = 1
 
+
     def apply_operator(self, key):
         np = self.np
         U = self.construct_operator(key)
         self._circuit_state = np.dot(U, self._circuit_state)
+
 
     def construct_operator(self, operator_key):
         np = self.np
@@ -74,8 +79,67 @@ class Circuit:
             self._operator_cache[operator_key] = operator_U
         return operator_U
 
+
     def apply_key_preprocessing(self, operator_key):
-        return operator_key
+        ## Tokenization
+        # Remove spaces at start and end
+        stripped_key = operator_key.strip(" ")
+        # Split operator into tokens
+        tokenized_key = re.split(" ", stripped_key)
+
+        ## Validation
+        for token in tokenized_key:
+            if len(token) < 2:
+                raise ValueError(f"Invalid gate provided: Received {token}, expected a gate of length at least 2")
+
+            gate = token[0]
+            if gate not in self.SINGLE_QUBIT_GATES:
+                raise ValueError(f"Invalid gate provided: Received {token}, {gate} cannot be resolved to a valid gate")
+
+        ## Ordering
+        # Tokenize into tuples
+        aux_gates = []
+        for token in tokenized_key:
+            aux_gates.append((token[0], int(token[1:])))
+        # Use insertion sort to sort the tuples
+        for i in range(1, len(aux_gates)):
+            selected_gate = aux_gates[i]
+            j = i - 1
+            while j >= 0 and aux_gates[j][1] > selected_gate[1]:
+                aux_gates[j + 1] = aux_gates[j]
+                j -= 1
+            aux_gates[j + 1] = selected_gate
+
+        ## Padding
+        # Pad empty space with I gates
+        gate_cursor = 0
+        gates = []
+        for gate in aux_gates:
+            # Pad before gate with identity gates
+            while (gate_cursor < gate[1]):
+                gates.append(("I", gate_cursor))
+                gate_cursor += 1
+                
+            # Add gate to list
+            gates.append((gate[0], gate[1]))
+            gate_cursor += 1
+
+        # gate_cursor should not be larger than the number of qubits. If so, this means more gates have been added than there are qubits in the circuit
+        if gate_cursor > self._qubits:
+            raise ValueError("Operator has more gates than there are qubits in the circuit, invalid shape. Qubits in circuit is " + str(self._qubits))
+
+        # Pad with identity gates after all gates have been added
+        while gate_cursor < self._qubits:
+            gates.append(("I", gate_cursor))
+            gate_cursor += 1
+
+        ## Parse tuples back into a string
+        output_key = gates[0][0] + str(gates[0][1])
+        for i in range(1, len(gates)):
+            output_key += " " + gates[i][0] + str(gates[i][1])
+    
+        return output_key
+
 
     def parse_operator_key(self, operator_key):
         gate_cursor = 0
@@ -84,11 +148,11 @@ class Circuit:
 
         for token in tokenized_key:
             if len(token) < 2:
-                raise ValueError("Invalid gate provided")
+                raise ValueError(f"Invalid gate provided: Received {token}, expected a gate of length at least 2")
 
             gate = token[0]
             if gate not in self.SINGLE_QUBIT_GATES:
-                raise ValueError("Invalid gate provided")
+                raise ValueError(f"Invalid gate provided: Received {token}, {gate} cannot be resolved to a valid gate")
             gate_index = int(token[1:])
 
             # Pad before gate with identity gates
@@ -110,6 +174,7 @@ class Circuit:
             gate_cursor += 1
 
         return gates    
+
 
     def measure(self):
         np = self.np
