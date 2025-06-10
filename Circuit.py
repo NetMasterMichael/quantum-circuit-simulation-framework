b7b67e488e20761d7de1f1183ca397e1438c112a
Michael Goodwin <michael.cgdwn@gmail.com>
2025-06-10 17:06:24 +0100

    Reorder functions in Circuit to improve readability

--- Circuit.py
+++ Circuit.py
@@ -46,6 +46,37 @@ class Circuit:
         self._circuit_state = np.zeros(2 ** self._qubits, dtype = complex)
         self._circuit_state[0] = 1
 
+    def apply_operator(self, key):
+        np = self.np
+        U = self.construct_operator(key)
+        self._circuit_state = np.dot(U, self._circuit_state)
+
+    def construct_operator(self, operator_key):
+        np = self.np
+        # If operator U is already cached, skip
+        processed_operator_key = self.apply_key_preprocessing(operator_key)
+        if processed_operator_key in self._operator_cache:
+            return self._operator_cache[processed_operator_key]
+        # Parse string to array of operators
+        operator_construction = self.parse_operator_key(processed_operator_key)
+        # Start with 1 dimensional identity matrix
+        operator_U = np.array([1], dtype = complex)
+        # Construct U by tensoring gates together into one matrix
+        for gate in operator_construction:
+            if gate[0] in self.SINGLE_QUBIT_GATES:
+                operator_U = np.kron(operator_U, self.SINGLE_QUBIT_GATES[gate[0]])
+            else:
+                # Gate not recognized, so print warning and tensor I matrix
+                print("WARNING: Gate " + gate[0] + " not recognized, substituting for I")
+                operator_U = np.kron(operator_U, self.IDENTITY)
+        # If cache is enabled, then add it to the cache
+        if self._operator_cache_state:
+            self._operator_cache[operator_key] = operator_U
+        return operator_U
+
+    def apply_key_preprocessing(self, operator_key):
+        return operator_key
+
     def parse_operator_key(self, operator_key):
         gate_cursor = 0
         gates = []
@@ -78,38 +109,7 @@ class Circuit:
             gates.append(("I", gate_cursor))
             gate_cursor += 1
 
-        return gates
-
-    def apply_key_preprocessing(self, operator_key):
-        return operator_key
-
-    def construct_operator(self, operator_key):
-        np = self.np
-        # If operator U is already cached, skip
-        processed_operator_key = self.apply_key_preprocessing(operator_key)
-        if processed_operator_key in self._operator_cache:
-            return self._operator_cache[processed_operator_key]
-        # Parse string to array of operators
-        operator_construction = self.parse_operator_key(processed_operator_key)
-        # Start with 1 dimensional identity matrix
-        operator_U = np.array([1], dtype = complex)
-        # Construct U by tensoring gates together into one matrix
-        for gate in operator_construction:
-            if gate[0] in self.SINGLE_QUBIT_GATES:
-                operator_U = np.kron(operator_U, self.SINGLE_QUBIT_GATES[gate[0]])
-            else:
-                # Gate not recognized, so print warning and tensor I matrix
-                print("WARNING: Gate " + gate[0] + " not recognized, substituting for I")
-                operator_U = np.kron(operator_U, self.IDENTITY)
-        # If cache is enabled, then add it to the cache
-        if self._operator_cache_state:
-            self._operator_cache[operator_key] = operator_U
-        return operator_U
-
-    def apply_operator(self, key):
-        np = self.np
-        U = self.construct_operator(key)
-        self._circuit_state = np.dot(U, self._circuit_state)
+        return gates    
 
     def measure(self):
         np = self.np
