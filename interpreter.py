34536330c0f458077725a2cbd077865592772d91
Michael Goodwin <michael.cgdwn@gmail.com>
2025-09-26 23:44:22 +0100

    Implement basic stack execution in interpreter

--- interpreter.py
+++ interpreter.py
@@ -12,6 +12,11 @@ class Interpreter:
         file_execution_mode = False
         self.arguments = sys.argv[1:]
         self._execution_stack = []
+        self._circuit = None
+        self._circuit_init = False
+        self._qubits = None
+        self._runs = 1
+        self._show_style = None
         
         # Start main interpreter loop
         self.main()
@@ -55,7 +60,40 @@ class Interpreter:
 
             self._execution_stack.append(instruction)
 
-        print(self._execution_stack)
+        # Execute the program
+        execution_substack = []
+        for instruction in self._execution_stack:
+            match instruction.opcode:
+                case Opcode.QUBITS:
+                    self._qubits = instruction.operand
+                case Opcode.RUNS:
+                    self._runs = instruction.operand
+                case Opcode.INIT:
+                    self._circuit = Circuit(self._qubits)
+                case Opcode.APPLY:
+                    execution_substack.append(instruction)
+                case Opcode.MEASURE:
+                    execution_substack.append(instruction)
+                case Opcode.SHOW:
+                    self._show_style = instruction.operand
+        
+        results = []
+
+        for i in range(0, self._runs):
+            self._circuit.reset_circuit_state()
+            for instruction in execution_substack:
+                match instruction.opcode:
+                    case Opcode.APPLY:
+                        self._circuit.apply_operator(instruction.operand)
+                    case Opcode.MEASURE:
+                        self._circuit.measure()
+            
+            results.append(self._circuit.DEBUG_get_circuit_state())
+
+        # Show style not yet implemented, just print for now
+        print(results)
+
+        
 
 if __name__ == "__main__":
     Interpreter()
